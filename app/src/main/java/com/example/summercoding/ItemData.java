89b12947aa716f2208d1bd8475e673ee547a5f2e
jiheon <kjhkjh3922@naver.com>
2019-05-15 19:26:34 +0900

    Database Complete

---
+++ app/src/main/java/com/example/summercoding/ItemData.java
@@ -0,0 +1,15 @@
+package com.example.summercoding;
+
+public class ItemData {
+    public int _id;
+    public String title;
+    public String date;
+    public String memo;
+
+    public ItemData(int _id, String title, String date, String memo) {
+        this._id = _id;
+        this.title = title;
+        this.date = date;
+        this.memo = memo;
+    }
+}
